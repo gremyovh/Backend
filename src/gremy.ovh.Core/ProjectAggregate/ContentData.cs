fa496e2470efdbab4395d3946957bcbb7a91e6e0
Miguel Gremy <37306845+Miguel-Gremy@users.noreply.github.com>
2022-09-11 14:20:31 +0200

    Merge : Add id for navigation (#22)

--- src/gremy.ovh.Core/ProjectAggregate/ContentData.cs
+++ src/gremy.ovh.Core/ProjectAggregate/ContentData.cs
@@ -1,14 +1,16 @@
 ﻿using Ardalis.GuardClauses;
 using gremy.ovh.SharedKernel;
+using gremy.ovh.SharedKernel.Interfaces;
 using Newtonsoft.Json;
 
 namespace gremy.ovh.Core.ProjectAggregate;
-public class ContentData : EntityBase
+public class ContentData : EntityBase, IAggregateRoot
 {
   [JsonProperty(PropertyName = "ContentDataId")]
   public override int Id { get; set; }
   public string FileName { get; set; }
   public Post Post { get; set; }
+  public int PostId { get; set; }
 
   public void Update(
     string? fileName)
@@ -16,12 +18,13 @@ public class ContentData : EntityBase
     FileName = Guard.Against.NullOrEmpty(fileName);
   }
 
-  public ContentData(string fileName)
+  public ContentData(string fileName, int postId)
   {
     FileName = fileName;
+    PostId = postId;
     Post = new Post();
   }
-  public ContentData() : this(string.Empty)
+  public ContentData() : this(string.Empty, -1)
   {
   }
 }
